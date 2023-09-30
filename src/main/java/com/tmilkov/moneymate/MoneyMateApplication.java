424bcd98666812de28c29893a42a36507251f9fe
Toma Milkov <tmilkov@appolica.com>
2023-09-30 17:03:16 +0200

    Added spring boot project.

---
+++ src/main/java/com/tmilkov/moneymate/MoneyMateApplication.java
@@ -0,0 +1,13 @@
+package com.tmilkov.moneymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMateApplication.class, args);
+	}
+
+}
