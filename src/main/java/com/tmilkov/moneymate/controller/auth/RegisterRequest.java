207aca121faf52bff701a9002fd80a4bea2630fc
Toma Milkov <tmilkov@appolica.com>
2023-10-04 14:06:22 +0200

    Added validation for register fields.

--- src/main/java/com/tmilkov/moneymate/controller/auth/RegisterRequest.java
+++ src/main/java/com/tmilkov/moneymate/controller/auth/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.tmilkov.moneymate.controller.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +14,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 }
